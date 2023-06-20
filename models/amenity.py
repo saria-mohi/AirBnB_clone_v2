a30252ffcd2f254ff67c51b00a80615cd6c317c0
saria-mohi <saria.m.mohammed@gmail.com>
2023-06-20 18:24:49 +0300

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-"""Amenity Class Module"""
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class Amenity(BaseModel):
-    """Amenity Class Attributs"""
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    Amenity class inherits from BaseModel and Base
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                       back_populates="amenities")
--- models/review.py
+++ models/review.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Review Class Module"""
-from models.base_model import BaseModel
+""" Review module for the HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
-    """Review Class Attributs"""
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review class to store review information """
+    __tablename__ = "reviews"
+    text = Column(String(1024),
+                  nullable=False)
+    place_id = Column(String(60),
+                      ForeignKey("places.id", ondelete="CASCADE"),
+                      nullable=False)
+    user_id = Column(String(60),
+                     ForeignKey("users.id", ondelete="CASCADE"),
+                     nullable=False)
