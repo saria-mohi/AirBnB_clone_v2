e15a42e3f36f99a6f87d38a1f27c736ca8fccf1a
saria-mohi <saria.m.mohammed@gmail.com>
2023-06-20 22:09:38 +0300

    model

--- models/user.py
+++ models/user.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" holds class User"""
+import hashlib
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship, backref
+from os import getenv
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place",
-                          cascade="all,delete",
-                          backref=backref("user", cascade="all,delete"),
-                          passive_deletes=True,
-                          single_parent=True)
-    reviews = relationship("Review",
-                           cascade="all,delete",
-                           backref=backref("user", cascade="all,delete"),
-                           passive_deletes=True,
-                           single_parent=True)
+    """Representation of a user """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128),
+                       nullable=False)
+        _password = Column('password',
+                           String(128),
+                           nullable=False)
+        first_name = Column(String(128),
+                            nullable=True)
+        last_name = Column(String(128),
+                           nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="all, delete-orphan")
+    else:
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = pwd
