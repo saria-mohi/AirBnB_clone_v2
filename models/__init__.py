fbe092b1e851dccb4f4b1d4a4e500dc45a89b494
saria-mohi <saria.m.mohammed@gmail.com>
2023-06-21 12:43:06 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""Create a unique storage instance for your application"""
+"""Instantiates a storage object.
 
-from os import environ
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
+"""
+from os import getenv
 
-# check envirn var to determine storage method
-if environ['HBNB_TYPE_STORAGE'] == 'db':
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
-
-else:  # file storage selected
+else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
