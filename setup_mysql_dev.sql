b51020bf794220234746e47dcf9365bf61f2a27e
saria-mohi <saria.m.mohammed@gmail.com>
2023-06-21 11:54:00 +0300

    back to the files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,7 @@
--- Prepares a MySQL server for the project.
+-- Creates the database hbnb_dev_db with specified parameters
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
