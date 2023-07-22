5f81cead626d571e05af81c50aee7966cce0d94c
saria2016 <saria.m.mohammed@gmail.com>
2023-07-22 20:06:44 +0300

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Second task on flask"""
+from flask import Flask
+app = Flask(__name__)
+
+
+# this rout by default called when run it
+@app.route("/", strict_slashes=False)
+def display():
+    # this is the method to dispaly message for user
+    return("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def show_message():
+    # this is the method2 to dispaly message for user
+    return("HBNB")
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_C(text):
+    # this is the method2 to dispaly message for user
+    return("C {}".format(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
