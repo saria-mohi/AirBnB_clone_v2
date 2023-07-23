3debfcda6c9be4a58b790a012abec87decbba707
saria2016 <saria.m.mohammed@gmail.com>
2023-07-23 16:20:29 +0300

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,39 +1,26 @@
 #!/usr/bin/python3
-from flask import Flask, Response
-from flask import render_template
+""" task on flask with model"""
+from flask import Flask, render_template
 from models import storage
-"""
-Very Simple Flask hello world
-"""
 
 
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states')
-def cities_by_states():
-    """
-    Inserts all Cities in each State from the database to the DOM
-    """
-    storall = storage.all("State").values()
-    return (Response(render_template(
-        '8-cities_by_states.html',
-        states=storall))
-        )
+def state_list():
+    # Inserts all States
+    storall_state = storage.all("State").values()
+    return (render_template('7-states_list.html', states=storall_state))
 
 
 @app.teardown_appcontext
 def teardown(exception):
     """
-    Tears down the db connection
+    Tearsdown the db connection
     """
     storage.close()
 
 
-@app.after_request
-def add_headers(response):
-    response.headers['Server'] = "apache/2.4.7"
-    return (response)
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run("0.0.0.0", port=5000)
