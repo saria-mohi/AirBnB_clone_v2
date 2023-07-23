c8d38c804f25b288b0845db1f7e886ed49e27d26
saria2016 <saria.m.mohammed@gmail.com>
2023-07-23 17:16:01 +0300

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,13 +8,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def state_list():
-    # Inserts all States
-    storall_state = storage.all(State).values()
-    return (render_template('7-states_list.html', states=storall_state))
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def state_list():
     # Inserts all States
