3f5f286a038ed8c5f020913aa7a095637055a33d
saria2016 <saria.m.mohammed@gmail.com>
2023-07-23 20:54:11 +0300

    hbnb filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" task on flask with model"""
+from flask import Flask, render_template
+from models import storage
+# from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """display a HTML page like 6-index.html from static"""
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    Tearsdown the db connection
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
